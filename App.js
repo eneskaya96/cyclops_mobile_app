32e1e115b4c4d25af14f142a92b692d49c7d5f6c
Yaşar Özyurt <yasar.ozyurt@bilgeadam.com>
2023-12-14 13:45:08 +0300

    feat:Onesignal implementation completed.

--- App.js
+++ App.js
@@ -1,15 +1,51 @@
 import { View, StyleSheet, Image, TouchableOpacity, Text } from 'react-native';
-import { NotificationComponent }  from './src/components/NotificationComponent'
 import FontAwesome from 'react-native-vector-icons/FontAwesome'; 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { PanelComponent } from './src/components/PanelComponent'
 import { SocketProvider } from './src/services/socketProvider';
+import { OneSignal } from 'react-native-onesignal';
+import { ENDPOINT } from './constant';
 
+OneSignal.initialize("b62676ae-b664-47eb-90f1-02cf8ce4bce3");
 
 export default function App() {
-  
   const [selectedMenu, setSelectedMenu] = useState('DETECTIONS'); 
+
+   const sendTokenToBackend = async (token) => {
+    try {
+    
+        const response = await fetch(`${ENDPOINT}/api/save-token`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token: token }),
+        });
+      
+      console.log("response", response )
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+  
+      const data = await response.json();
+      console.log('Token sent successfully:', data);
+    } catch (error) {
+      console.error('There was a problem sending the token:', error);
+    }
+  }
+
+  useEffect(() => {
+    const handleOneSignal = async () => {
+        OneSignal.Notifications.requestPermission(true);
+        const userId = await OneSignal.User.pushSubscription.getPushSubscriptionId();
+        console.log(userId)
+        sendTokenToBackend(userId);
+
+    }
+    handleOneSignal()
+  }, [])
+  
   return (
     <View
       style={{
@@ -23,7 +59,6 @@ export default function App() {
           <Menu setSelectedMenu={setSelectedMenu} selectedMenu={selectedMenu}/>
           <PanelComponent selectedMenu={selectedMenu} />
           <AddButton/>
-          <NotificationComponent/>
         </SocketProvider>
     </View>
   );
